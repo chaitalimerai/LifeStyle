1b3e3d2c9a4fd88cb2c857eda9544f024c1ca5c7
unknown <chaitalimerai143@gmail.com>
2019-12-24 19:48:45 +0530

    feat: all files added

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 200) {
+        $('.top-nav').addClass('fixed-header');
+    }
+    else {
+        $('.top-nav').removeClass('fixed-header');
+    }
+});
+
+if ($(window).width() >= 728) {
+    $(".navbar-toggle").click(function (e) {
+        $(".nav").slideToggle(300);
+        e.preventDefault();
+    });
+}
+
+
+
+
